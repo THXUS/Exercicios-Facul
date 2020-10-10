995b1a9e863ebf0b8660666a222d159f7d546b0f
Matheus Santana <matheus11_santana@hotmail.com>
2020-10-10 15:24:11 -0300

    estudo sobre a função de leitura put

--- Exercicios-facul/teste/teste.cpp
+++ Exercicios-facul/teste/teste.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    cout << "Teste" << endl;
+    cout.put('a'); //alternativa de cout para imprimir char
+    cout << 'c';
+    cout.put('abd'); //se colocarmos mais de um caractere (string) a função irá ler apenas o último caractere
+    cout << "\n";
+    cout << setw(10); //próximo linha começara numa largura de 10px
+    cout.put('g');    //a função put ignora esses tipos de falha
+    cout << "\n";
+    cout.put('a').put('b').put('c').put('d').put('.').put('.').put('.'); //concatenação usando put
 }
